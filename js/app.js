3e657f31d24363fc5bad92ec4ad31cfbb79f626d
Gabriel Jimenez <gabe.jimenez@outlook.com>
2019-03-05 15:40:23 -0700

    Update to ALPHA

--- js/app.js
+++ js/app.js
@@ -2,5 +2,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/site.css';
 import ES6Lib from './es6codelib';
+import 'bootstrap/js/src/collapse.js';
+//import { library, icon } from '@fortawesome/fontawesome-svg-core'
 
-module.hot.accept();
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+//import { faCheck } from "@fortawesome/free-regular-svg-icons/faAddressBook";
+
+
+
+library.add(fas, far, fab) 
+//library.add(faCheck);
+//library.add(far, fab) 
+//library.add(faCheck);
+dom.watch();
+//dom.i2svg()
+
+//import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+//library.add(faIgloo)
+
+//const igloo = icon({ prefix: 'fas', iconName: 'igloo' })
+
+
+//module.hot.accept();
--- js/profitability-simulator.js
+++ js/profitability-simulator.js
@@ -7,6 +7,7 @@
   calcStaffSalaryWages();
   var cD8 = new Intl.NumberFormat().format(parseInt($('#d8').val().replace(/,/g, '')))
   $('#d8').val(cD8);
+  recalcAllScenarios();
 });
 $('#d9').change(function() {
   calcTransactionsPerFTEVeternarian();
@@ -14,39 +15,46 @@ $('#d9').change(function() {
   calcAnnualTransactionsPerActiveClient();
   var cD9 = new Intl.NumberFormat().format(parseInt($('#d9').val().replace(/,/g, '')))
   $('#d9').val(cD9);
+  recalcAllScenarios();
 });
 $('#d10').change(function() {
   calcRevenuePerFTEVeternarian();
   calcTransactionsPerFTEVeternarian();
   calcActiveClientsPerFTEVeternarian();
   var cD10 = new Intl.NumberFormat().format(parseInt($('#d10').val().replace(/,/g, '')))
-  $('#d10').val(cD10);
+  $('#d10').val(new Intl.NumberFormat().format(parseFloat((d10).toFixed(2))));
+  recalcAllScenarios();
 });
 $('#d11').change(function() {
   calcRevenuePerFTONonVeternarianEmp();
   var cD11 = new Intl.NumberFormat().format(parseInt($('#d11').val().replace(/,/g, '')))
-  $('#d11').val(cD11);
+  $('#d11').val(new Intl.NumberFormat().format(parseFloat((d11).toFixed(2))));
+  recalcAllScenarios();
 });
 $('#d12').change(function() {
   calcActiveClientsPerFTEVeternarian();
   calcAnnualTransactionsPerActiveClient();
   var cD12 = new Intl.NumberFormat().format(parseInt($('#d12').val().replace(/,/g, '')))
   $('#d12').val(cD12);
+  recalcAllScenarios();
 });
 $('#d13').change(function() {
   calcDrugsSuppliesExpense();
   var cD13 = new Intl.NumberFormat().format(parseInt($('#d13').val().replace(/,/g, '')))
   $('#d13').val(cD13);
+  recalcAllScenarios();
 });
 $('#d14').change(function() {
   calcLaboratoryExpense();
   var cD14 = new Intl.NumberFormat().format(parseInt($('#d14').val().replace(/,/g, '')))
   $('#d14').val(cD14);
+  recalcAllScenarios();
 });
 $('#d15').change(function() {
   calcStaffSalaryWages();
   var cD15 = new Intl.NumberFormat().format(parseInt($('#d15').val().replace(/,/g, '')))
   $('#d15').val(cD15);
+  recalcAllScenarios();
 });
 $('#d19').change(function() {
   calcPotentialEffectOnTotalIncome_1();
@@ -55,10 +63,20 @@ $('#d20').change(function() {
   calcPotentialEffectOnTotalIncome_2();
 });
 $('#d34').change(function() {
-  calcPotentialEffectOnTotalIncome_1();
+  if($('#d34').val()>0){
+    calcPotentialEffectOnTotalIncome_1();
+  }
+  else{
+    $('#d34').val("0");
+  }
 });
 $('#d39').change(function() {
-  calcPotentialEffectOnTotalIncome_2();
+  if($('#d39').val()>0){
+    calcPotentialEffectOnTotalIncome_2();
+  }
+  else{
+    $('#d39').val("0");
+  }
 });
 $('#d46').change(function() {
   calcPotentialEffectOnTotalIncome_3();
@@ -69,6 +87,49 @@ $('#d53').change(function() {
 $('#d58').change(function() {
   calcPotentialEffectOnTotalIncome_5();
 });
+$('.aaha-profsim-enter').click(function() {
+    $('.aaha-profsim-results').removeClass('hide').addClass('show');
+    $('.aaha-profsim-main').removeClass('show').addClass('hide');
+});
+$('.aaha-profsim-exit').click(function() {
+    $('.aaha-profsim-results').removeClass('show').addClass('hide');
+    $('.aaha-profsim-main').removeClass('hide').addClass('show');
+});
+
+$(".form-control").on('keyup', function(evt){
+  FormatCurrency(this);
+});
+$(".form-control").on('keypress', function(evt){
+  return CheckNumeric();
+});
+
+function FormatCurrency(ctrl) {
+    //Check if arrow keys are pressed - we want to allow navigation around textbox using arrow keys
+    if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
+        return;
+    }
+
+    var val = ctrl.value;
+
+    val = val.replace(/,/g, "")
+    ctrl.value = "";
+    val += '';
+    x = val.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+
+    var rgx = /(\d+)(\d{3})/;
+
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+
+    ctrl.value = x1 + x2;
+}
+
+function CheckNumeric() {
+    return event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode == 46;
+}
 
 function calcRevenuePerFTEVeternarian(){
   if($('#d8').val() !== '' && $('#d10').val() !== '' ){
@@ -112,6 +173,23 @@ function calcAnnualTransactionsPerActiveClient(){
     $('#d23').val(new Intl.NumberFormat().format(parseFloat((d9 / d12).toFixed(2))));
   }
 }
+$(".aaha.card-header").click(function() {
+  var el = $(this).find("svg");
+  if(el.hasClass('fa-chevron-up')){ 
+    el.addClass('fa-chevron-down').removeClass('fa-chevron-up');
+  } else { 
+    el.addClass('fa-chevron-up').removeClass('fa-chevron-down'); 
+  }
+});
+$('div[data-toggle="collapse"]').click(function(e){
+    //e.stopPropagation();
+});
+function recalcAllScenarios(){
+  calcPotentialEffectOnTotalIncome_1();
+  calcPotentialEffectOnTotalIncome_2();
+  calcPotentialEffectOnTotalIncome_3();
+  calcPotentialEffectOnTotalIncome_4();
+}
 
 //CALC EXPENSES
 function calcDrugsSuppliesExpense(){
@@ -152,9 +230,10 @@ function calcPotentialEffectOnTotalIncome_2(){
     var d41 = parseFloat($('#d41').val().replace(/,/g, ''));
     var d26 = parseFloat($('#d26').val().replace(/,/g, ''));
     var d27 = parseFloat($('#d27').val().replace(/,/g, ''));
+    var d28 = parseFloat($('#d28').val().replace(/,/g, ''));
     $('#d41').val(new Intl.NumberFormat().format(Math.round(d39 * d20 * 52).toFixed(2)));
     var d41 = parseFloat($('#d41').val().replace(/,/g, ''));
-    $('#d42').val(new Intl.NumberFormat().format(Math.round(d41 * (1 - (d26/100) - (d27/100))).toFixed(2)));
+    $('#d42').val(new Intl.NumberFormat().format(Math.round(d41 * (1 - (d26/100) - (d27/100) - (d28/100))).toFixed(2)));
   }
 }
 function calcPotentialEffectOnTotalIncome_3(){
