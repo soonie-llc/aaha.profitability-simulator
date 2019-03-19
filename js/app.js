c7e0591031dbc52df120a1546f9be70cd0918549
Gabriel Jimenez <gabe.jimenez@outlook.com>
2019-03-19 03:45:34 -0600

    Optimized app.js/webpack
    Optimized app.js import for FontAwesome

--- js/app.js
+++ js/app.js
@@ -3,20 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/site.css';
 import ES6Lib from './es6codelib';
 import 'bootstrap/js/src/collapse.js';
-//import { library, icon } from '@fortawesome/fontawesome-svg-core'
+
+//import { library, dom } from '@fortawesome/fontawesome-svg-core'
+//import { fas } from '@fortawesome/free-solid-svg-icons'
+//import { far } from '@fortawesome/free-regular-svg-icons'
+//import { fab } from '@fortawesome/free-brands-svg-icons'
+//library.add(fas, far, fab) 
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-//import { faCheck } from "@fortawesome/free-regular-svg-icons/faAddressBook";
-
-
-
-library.add(fas, far, fab) 
-//library.add(faCheck);
-//library.add(far, fab) 
-//library.add(faCheck);
+import { faPencilAlt, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+library.add(faPencilAlt, faChevronDown, faChevronUp);
 dom.watch();
 //dom.i2svg()
 
